6d951b86bb0dc0f9b399b5d3ff6485f5e785c318
Cook1619 <cook6348@gmail.com>
2020-08-09 12:13:02 -0500

    Adding stateless functional component for my dummy component

--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import Proptypes from "prop-types";
 
-export default class Movie extends Component {
-  static propTypes = {
-    movie: Proptypes.shape({
-      title: Proptypes.string.isRequired,
-      overview: Proptypes.string.isRequired,
-    }),
-  };
-  render() {
-    const { title, overview } = this.props.movie;
+// export default class Movie extends Component {
+//   static propTypes = {
+//     movie: Proptypes.shape({
+//       title: Proptypes.string.isRequired,
+//       overview: Proptypes.string.isRequired,
+//     }),
+//   };
+//   render() {
+//     const { title, overview } = this.props.movie;
 
-    return (
-      <div>
-        <h3>{title}</h3>
-        <p>{overview}</p>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div>
+//         <h3>{title}</h3>
+//         <p>{overview}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const Movie = ({ movie }) => (
+  <div>
+    <h3>{movie.title}</h3>
+    <p>{movie.overview}</p>
+  </div>
+);
+
+Movie.propTypes = {
+  movie: Proptypes.shape({
+    title: Proptypes.string.isRequired,
+    overview: Proptypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Movie;
